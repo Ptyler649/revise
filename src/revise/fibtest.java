17852c3ea04334c32f2d4404a1c7f5e4e8adbcf7
ptyler649 <ptyler649@gmail.com>
2022-01-10 16:38:30 +0000

    initial commit for testing

---
+++ src/revise/fibtest.java
@@ -0,0 +1,21 @@
+package revise;
+
+public class fibtest {
+
+	public static void main(String[] args) {
+				
+				int a=0, b=0, c=1;
+				
+				for(int count = 0; count <=10; count++) {
+					a=b;
+					b=c;
+					c=a+b;
+					System.out.println(a);
+				}
+				
+				// you can also do this using recusion
+				
+			}
+
+	}
+
